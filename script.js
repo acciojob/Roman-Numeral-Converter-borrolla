df5955ae7f51362f24fa8db146f61d2a0d37f5ff
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-03-14 18:02:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ function convertToRoman(num) {
     };
   //your code here
 	let roman = "",
-		symbol.forEach((item)) => {
+		symbols.forEach((item)) => {
 		   const [symbol, value] = item;
 		   while(num >= value)
 			   {
 				   roman += symbol;
 				   num -= value;
 			   }
-		})
+		});
     return roman;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
